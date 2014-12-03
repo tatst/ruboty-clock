20e84e1a22c5e417cf314eb2f7b00927c35eb7f1
Tatsuya Toyoda <b8eansatz@gmail.com>
2014-12-03 16:52:31 +0900

    Correct clock.rb

--- clock.rb
+++ clock.rb
@@ -3,13 +3,13 @@ module Ruboty
     class Clock < Base
       on (
         /今何時/, 
-	name: 'clock',
-	description: '現在時刻を返します'
+	name: "clock",
+	description: "現在時刻を返します"
       )
 
       def clock(message)
         now = Time.now
-	message.reply('#{now.hour}:#{now.min}')
+	message.reply("#{now.hour}:#{now.min}")
       end
     end
   end
